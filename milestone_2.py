b9062c1842ddf8464598f9b67e533c3378a3612a
CodingElvis <davelewis0@hotmail.com>
2024-05-26 13:48:27 +0100

    Adds fist few lkines of code for milestone 2

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,17 @@
+import random
+
 word_list = ["apple", "banana", "peach", "strawberry", "pineapple"]
 
 print (word_list)
+
+word = random.choice(word_list)
+
+print(word)
+
+guess =input("Please enter the letter you wish to guess: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else: 
+    print ("Oops! That is not a valid guess")
+    
